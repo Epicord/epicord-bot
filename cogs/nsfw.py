ddcc274cdb55e9ede2e9b6bca125bafc42d0fab8
wingGryph <wingGryph@users.noreply.github.com>
2016-08-01 23:56:56 +0100

    Add gelbooru to nsfw cog

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -1,8 +1,11 @@
 import json
 import requests
+
 from discord.ext import commands
-from random import choice
 from .utils import checks
+
+from random import choice, randrange
+import xml.etree.ElementTree as ET
 
 
 class NSFW:
@@ -28,7 +31,9 @@ class NSFW:
                 "https://e621.net/post/index.json",
                 params=payload
             )
+
             posts = json.loads(e621_response.text)
+
             try:
                 selected_post = choice(posts)
 
@@ -37,7 +42,7 @@ class NSFW:
                         selected_post["id"]
                     )
                 else:
-                    e621_message = "{}\nhttps://e621.net/post/show/{}".format(
+                    e621_message = "{}\n<https://e621.net/post/show/{}>".format(
                             selected_post["file_url"],
                             selected_post["id"]
                         )
@@ -45,6 +50,33 @@ class NSFW:
 
             except IndexError:
                 await self.bot.say("Sorry, no results.")
+
+
+    @commands.command()
+    async def gelbooru(self, *tags: str):
+        """
+        For all your non-furry porn needs
+        """
+        payload = {
+            "limit": 100,
+            "tags": " ".join(tags)
+        }
+        gelbooru = requests.get(
+            "http://gelbooru.com//index.php?page=dapi&s=post&q=index",
+            params=payload
+        )
+
+        root = ET.fromstring(gelbooru.text)
+
+        try:
+            selected_post = root[randrange(len(root))]
+
+            await self.bot.say("{}\n<http://gelbooru.com/index.php?page=post&s=view&id={}>".format(
+                selected_post.attrib["file_url"],
+                selected_post.attrib["id"]
+            ))
+        except (IndexError, ValueError):
+            await self.bot.say("Sorry, no results.")
 
 
 def setup(bot):
