ecef244751f8318d580c683fb827ed03fc997ae9
wingGryph <wingGryph@users.noreply.github.com>
2016-08-11 15:41:06 +0100

    add general cog to startup

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from cogs.utils import checks
 bot = commands.Bot(command_prefix='~', description='''
                    EpicordBot - created in the advent of BooBot's demise.
                    ''', pm_help=None, help_attrs=dict(hidden=True))
-exts = ['cogs.nsfw', 'cogs.8x']
+exts = ['cogs.nsfw', 'cogs.8x', 'cogs.general']
 
 
 @bot.event
