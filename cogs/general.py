f8954f919439332d6484291d62f35c5600d9daa1
minimuss01 <ss01.wdl@gmail.com>
2016-08-15 21:22:18 -0500

    PEP8 compliance for cogs.general.py

--- cogs/general.py
+++ cogs/general.py
@@ -1,21 +1,18 @@
+# NOQA
 import random
-
 from discord.ext import commands
-from .utils import checks
 
 
 class General:
-    """
-    Commands that don't fit anywhere else
-    """
+    """Commands that don't fit anywhere else."""
+
     def __init__(self, bot):
+        """Cog constructor."""
         self.bot = bot
 
     @commands.command(aliases=["choose"])
     async def choice(self, *choices: str):
-        """
-        Selects one of the choices passed at random
-        """
+        """Select one of the choices passed at random."""
         choice_list = " ".join(choices).split(";")
         choice_str = ""
         for cur in choice_list:
@@ -27,9 +24,7 @@ I choose: {}!""".format(choice_str, random.choice(choice_list)))
 
     @commands.command()
     async def eball(self, *ask: str):
-        """
-        Replies to question with an emoji
-        """
+        """Reply to question with an emoji."""
         the_e_list = [
             ":heart_eyes_cat:",
             ":revolving_hearts:",
@@ -55,9 +50,7 @@ I choose: {}!""".format(choice_str, random.choice(choice_list)))
 
     @commands.command(name='8ball')
     async def eightball(self, *q: str):
-        """
-        Given a question, randomly answers it.
-        """
+        """Given a question, randomly answer it."""
         answers = [
             'It is certain.',
             'Of course.',
@@ -76,17 +69,20 @@ I choose: {}!""".format(choice_str, random.choice(choice_list)))
             'That\'s completely absurd.',
             'No, absolutely not! :thumbsdown:',
             'I don\'t know m8 ʅ(◔౪◔ ) ʃ',
-            ':crystal_ball: The answer is not what you think.'
+            ':crystal_ball: The answer is not what you think.',
+            '',
+            ''
         ]
         await self.bot.reply('`{}`: {}'.format(' '.join(q),
                                                random.choice(answers)))
 
     @commands.command(name='8user',
-                      brief='Given a question, randomly selects a user.')
+                      brief='Given a question, randomly select a user.')
     async def eightuser(self, *q: str):
-        """
-        Given a question, utilizes a neural network to analyze its intent
-        and intelligently selects the user that most completely fits the
+        """Given a question, randomly select a user.
+
+        This is done by utilizing a neural network to analyze its intent
+        and intelligently select the user that most completely fits the
         situation at hand.
         """
         user = random.choice(list(self.bot.get_all_members())).display_name
@@ -94,4 +90,5 @@ I choose: {}!""".format(choice_str, random.choice(choice_list)))
 
 
 def setup(bot):
+    """Setup function."""
     bot.add_cog(General(bot))
